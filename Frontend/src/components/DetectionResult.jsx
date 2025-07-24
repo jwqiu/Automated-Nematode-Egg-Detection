46bd10803b5e36cabb633167e77312773b703e91
Junwen-Qiu-1162541 <jwqiu1111@gmail.com>
2025-07-25 10:15:12 +1200

    Restructured project directories: separated frontend (my-app),  model code , and backend (backend)

--- Frontend/src/components/DetectionResult.jsx
+++ Frontend/src/components/DetectionResult.jsx
@@ -5,7 +5,7 @@ function DetectionResult() {
         <div className="bg-white shadow-lg flex flex-col rounded-lg p-8 flex-1 ">
             <div className="flex justify-between items-center mb-3">
                 <p className="mb-0 text-gray-500">Detection Result:</p>
-                <button className="bg-blue-500 text-white rounded-lg px-4 py-2">Detect</button>
+                <button className="bg-blue-500 text-white rounded-xl px-4 py-2">Detect</button>
             </div>
             <div className="w-full flex-1 bg-gray-50 p-8 text-md rounded text-center italic shadow-inner text-gray-400">No result</div>
         </div>
