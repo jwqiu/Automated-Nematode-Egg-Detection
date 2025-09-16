8546b1abe68c05bd876d00b690f1dd5d3aeb165a
jwqiu <jwqiu1111@gmail.com>
2025-09-16 15:23:19 +1200

    Update ImageUploader.jsx

--- frontend/src/components/ImageUploader.jsx
+++ frontend/src/components/ImageUploader.jsx
@@ -397,7 +397,7 @@ function ImageUploader({ images, setImages, setSelectedImage, selectedImage, bot
             <div>
                 <label
                 htmlFor="upload"
-                className="cursor-pointer font-bold inline-block bg-blue-500 text-white px-3 py-2 rounded-xl hover:bg-blue-600"
+                className="cursor-pointer font-bold inline-block bg-white border border-blue-500 text-blue-500 rounded-lg px-3 py-2 rounded-xl hover:bg-blue-500"
                 >
                     Upload
                 </label>
